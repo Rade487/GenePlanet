42ac385d1c4ffd214d55972faa394dd6f2c6098c
Rade Toprek <rade.toprek@gmail.com>
2021-10-26 11:39:11 +0200

    VcfReader

--- vcf_reader.py
+++ vcf_reader.py
@@ -1,9 +1,8 @@
 import io
-import os
 import pandas as pd
 
 
-def read_vcf(path):
+def read_vcf(path, num_of_rows):
     with open(path, 'r') as f:
         lines = [l for l in f if not l.startswith('##')]
     return pd.read_csv(
@@ -11,5 +10,14 @@ def read_vcf(path):
         dtype={'#CHROM': str, 'POS': str, 'ID': str, 'REF': str, 'ALT': str,
                'FORMAT': str},
         sep='\t',
-        nrows=100
+        nrows=num_of_rows
     ).rename(columns={'#CHROM': 'CHROM'})
+
+
+def convert_vcf_to_csv(data_frame):
+    return data_frame.to_csv('test.csv', index=False)
+
+
+def remove_space_from_csv(dataset):
+    trim = lambda x: x.strip() if type(x) is str else x
+    return dataset.applymap(trim)
