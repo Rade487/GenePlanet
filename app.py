461e56a713139b31bcc670e4eed65c135ac217dd
Danilo Dobras <danilodobras97@gmail.com>
2021-10-26 11:36:48 +0200

    endpoint submit

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
-from flask import Flask
+from flask import Flask, request,render_template
 from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
-from vcf_reader import read_vcf
+import re
+
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://newuser:pass@localhost/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+class Gene(db.Model):
+    tablename = 'gene'
+    id = db.Column(db.String(100), primary_key="True")
+    chrom = db.Column(db.String(200))
+    pos = db.Column(db.Integer)
+    ref = db.Column(db.String(100))
+    alt = db.Column(db.String(100))
+    format = db.Column(db.String(100))
+
+    def init(self, chrom,  pos, ref, alt, info):
+        self.chrom = chrom
+        self.pos = pos
+        self.ref = ref
+        self.alt = alt
+        self.info = info
 
 @app.route('/')
 def hello_world():
@@ -23,6 +40,21 @@ def import_vcf_into_db():
     df = pd.read_csv('vcf_example.csv')
     df.to_sql(name='gene', con='postgresql://newuser:pass@localhost/postgres')
     return 'TOP'
+
+@app.route('/submit')
+def find_by_search_input():
+    #input_text = request.form['search']
+    input_text = 'rs367896724'
+    if input_text.startswith('rs'):
+        row = Gene.query.filter_by(id=input_text).first()
+        return render_template('index.html', data=row)
+    elif re.match('[0-9] ', input_text):
+        input_parts = input_text.split(' ')
+        row = Gene.query.filter_by(chrom=input_parts[0]).filter_by(pos=input_parts[1]).first()
+        return render_template('index.html', data=row)
+    else:
+        return 'Not matched'
+
 
 if __name__ == '__main__':
     app.run()
--- model.py
+++ model.py
@@ -1,18 +1,4 @@
 from app import db
 
 
-class Gene(db.Model):
-    tablename = 'gene'
-    id = db.Column(db.String(100), primary_key="True")
-    chrom = db.Column(db.String(200))
-    pos = db.Column(db.Integer)
-    ref = db.Column(db.String(100))
-    alt = db.Column(db.String(100))
-    info = db.Column(db.String(100))
 
-    def init(self, chrom,  pos, ref, alt, info):
-        self.chrom = chrom
-        self.pos = pos
-        self.ref = ref
-        self.alt = alt
-        self.info = info
